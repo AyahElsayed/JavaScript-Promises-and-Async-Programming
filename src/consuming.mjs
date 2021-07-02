716e7f72000490449bfe8283d23738ef86e1a478
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-02 17:41:06 +0200

    handling errors

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,14 +1,17 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, { appendText, showWaiting, hideWaiting } from "./results.mjs";
 
 // promises are not lazy
 export function get() {
     axios.get("http://localhost:3000/orders/1")
-        .then(({data}) => setText(JSON.stringify(data)));
+        .then(({ data }) => setText(JSON.stringify(data)));
     // this destructuring mean that axios will return {data}
     // then mean => after the get complete run this code
 }
 
 export function getCatch() {
+    axios.get("http://localhost:3000/orders/123")
+        .then(({ data }) => setText(data.id))
+        .catch((err) => setText(err));
 }
 
 export function chain() {
