be4f57ee331100d8ad8fc4738501bbadc5123eb4
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-02 17:31:34 +0200

    consuming promises

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,7 +1,11 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
+// promises are not lazy
 export function get() {
-    axiox.get(("http://localhost:3000/orders/1"))
+    axios.get("http://localhost:3000/orders/1")
+        .then(({data}) => setText(JSON.stringify(data)));
+    // this destructuring mean that axios will return {data}
+    // then mean => after the get complete run this code
 }
 
 export function getCatch() {
