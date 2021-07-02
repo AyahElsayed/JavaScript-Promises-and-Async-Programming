04fb2d48fe1c685b4770b51e39f839b8fe19c17f
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-02 21:11:59 +0200

    creating promises

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,22 +1,35 @@
 import setText, { appendText } from "./results.mjs";
 
-export function timeout(){
+// promise => 
+// object that represents the eventual completion (or failure) of an asynchronous operation, 
+// and its resulting value
+
+// let temp = new promise()  // => pending
+
+export function timeout() {
+    let wait = new Promise(resolve => {
+        setTimeout(() => {
+            resolve("Timeout!"); // update one time
+        }, 1500);
+    });
+
+    wait.then(setText);
 }
 
-export function interval(){
+export function interval() {
 }
 
-export function clearIntervalChain(){
+export function clearIntervalChain() {
 }
 
-export function xhr(){
+export function xhr() {
 }
 
-export function allPromises(){
+export function allPromises() {
 }
 
-export function allSettled(){
+export function allSettled() {
 }
 
-export function race(){
+export function race() {
 }
