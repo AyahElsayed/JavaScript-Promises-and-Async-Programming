28659db48fcdf6ef27a1b72ab4966f14815b6947
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-02 21:28:33 +0200

    rejecting a promise

--- src/creating.mjs
+++ src/creating.mjs
@@ -44,6 +44,23 @@ export function clearIntervalChain() {
 }
 
 export function xhr() {
+    let request = new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:3000/users/7");
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        xhr.onerror = () => {
+            reject("Request failed!");
+        };
+        xhr.send();
+    });
+
+    request.then(setText).catch(setText);
 }
 
 export function allPromises() {
